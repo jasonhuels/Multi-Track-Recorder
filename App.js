af11064d45793931f45844d8b26d19d5f152a0cd
Jason Huels <jasonhuels@yahoo.com>
2019-12-08 12:36:33 -0800

    layout basic app buttons

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Dimensions, Slider } from 'react-native';
+import { Asset } from 'expo-asset';
+import { Audio } from 'expo-av';
+import * as Permissions from 'expo-permissions';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{
+          flexDirection: 'row'}}>
+          <Button title="Rec" />
+          <Button title="Solo" />
+          <Button title="Mute" />
+          {/* Volume Slider */}
+          <Slider/> 
+          
+        </View>
+        <View style={{
+          flexDirection: 'row'}}>
+          <Button title="Play" />
+          <Button title="Stop" />
+          {/* Master Volume Slider */}
+          <Slider /> 
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
